2b154ab618e49220c5cbf5095a7c8fa900945c17
Bhumi Prasad <prasad14bhumi@gmail.com>
2025-06-11 13:13:21 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Personal_Portfolio_Website
+A responsive and interactive portfolio built using HTML, CSS, and JavaScript. It showcases my projects, skills, and contact information in a clean, user-friendly layout. The website includes smooth scrolling, modern animations, and dynamic elements to enhance user experience. Designed to reflect my passion for web development and attention to detail.
---
+++ script.js
@@ -0,0 +1,44 @@
+let toggleMenu=document.getElementById("toggle-btn");
+let navlinks=document.querySelector(".nav_links")
+toggleMenu.addEventListener("click",()=>{
+    navlinks.classList.toggle("active");
+})
+let proj1=document.getElementById("card1");
+proj1.addEventListener("click",()=>{
+    window.open("https://weather-app-epir.vercel.app/")
+});
+let proj2=document.getElementById("card2");
+proj2.addEventListener("click",()=>{
+    window.open("https://rock-paper-scissors-game-psi-three.vercel.app/")
+});
+let proj3=document.getElementById("card3");
+proj3.addEventListener("click",()=>{
+    window.open("https://tic-tac-toe-gilt-theta.vercel.app/")
+});
+const contactForm = document.getElementById('contactForm');
+
+contactForm.addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  const name = document.getElementById('name').value.trim();
+  const email = document.getElementById('email').value.trim();
+  const message = document.getElementById('message').value.trim();
+
+  if (!name || !email || !message) {
+    alert('Fill all the details');
+    return;
+  }
+
+  if (!validateEmail(email)) {
+    alert('Please enter a valid email address');
+    return;
+  }
+
+  alert(`Thank you, ${name}! Your message has been sent to Bhumi.`);
+  contactForm.reset();
+});
+
+function validateEmail(email) {
+  const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return re.test(email);
+}
